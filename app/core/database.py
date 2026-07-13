98d9f11158811954adea3286e850e6eea45185d2
Illia Dovhaliuk <2201illia@gmail.com>
2026-07-13 16:04:04 +0300

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # SQLite для швидкого старту
 SQLALCHEMY_DATABASE_URL = "sqlite:///./fleet.db"
@@ -12,6 +12,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Створюємо Base тут, щоб інші моделі могли його імпортувати
 Base = declarative_base()
+
 
 # Залежність для отримання сесії БД в ендпоінтах
 def get_db():
